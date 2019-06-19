5374d427a9ac4a3491fb5482e769ea17b1905e4d
iammancy <iammancy@gmail.com>
2019-06-19 21:19:58 +0530

    added styling

--- generator.js
+++ generator.js
@@ -1,16 +1,16 @@
 
 function generateBooks(){
 
-var search = $("[name='book']");
+var search = $("[name='book']")[0].value;
 var title='';
-var searchUrl = "https://www.googleapis.com/books/v1/volumes/?q="+search
+var searchUrl = "https://www.googleapis.com/books/v1/volumes/?q="+search;
 $.ajax({url : searchUrl,
     type:'GET',
     dataType: 'jsonp',
     success:function(result){
     console.log(result);
     result.items.forEach(element => {
-    title =  $('<div>'+element.id+'</div>');
+    title =  $('<div class="position-relative col-6 ">'+element.volumeInfo.publisher+'</div>');
     title.appendTo('#parent');
     });
     }
